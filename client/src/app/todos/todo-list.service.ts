84e72cb5a247cc35bc5485fe5a29e7da8c30c5c3
Sungjae Park <park1637@morris.umn.edu>
2018-02-15 14:18:32 -0600

    Created todo-list.service.ts
    -David and Park-

---
+++ client/src/app/todos/todo-list.service.ts
@@ -0,0 +1,23 @@
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+
+import {Observable} from 'rxjs/Observable';
+
+import {environment} from '../../environments/environment';
+import {Todo} from "./todo";
+
+@Injectable()
+export class todoListService {
+  readonly todoUrl: string = environment.API_URL + 'todos';
+
+  constructor(private httpClient: HttpClient) {
+  }
+
+  getTodos(): Observable<Todo[]> {
+    return this.httpClient.get<Todo[]>(this.todoUrl);
+  }
+
+  getTodoById(_id: string): Observable<Todo> {
+    return this.httpClient.get<Todo>(this.todoUrl + '/' + _id);
+  }
+}
